a76815e1ade6dae0ce44f496a6ce3cbf86f8e568
disVill <konokou2000@gmail.com>
2023-02-05 00:57:50 +0900

    fix Q24

--- Q24/app/app/settings.py
+++ Q24/app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from decouple import config, Csv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#&ps$8cu!70bf8n)*zj1s2ssxzht_jc)1i(@qq^$1spe^yi-u4"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOST", cast=Csv())
 
 
 # Application definition
